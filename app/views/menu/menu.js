989aec464616379c988eff291215d8e93470a913
SDobrev <stoyan172@gmail.com>
2016-02-19 19:19:07 +0200

    ....

--- app/views/menu/menu.js
+++ app/views/menu/menu.js
@@ -1,4 +1,12 @@
 var frameModule = require("ui/frame");
+
+exports.loaded = function (argument) {
+		var page = argument.object;
+	if(page.ios) {
+		frameModule.Frame.defaultTransition = { name: "slide" };
+	}
+}
+
 exports.beersList = function(argument) {
 	var topmost = frameModule.topmost();
     topmost.navigate("views/list/list");
--- app/views/splash/splash.js
+++ app/views/splash/splash.js
@@ -5,11 +5,9 @@ exports.loaded = function (argument) {
 	var page = argument.object;
 	var image = page.getViewById("imageSplash");
 	if(page.ios) {
-		//image.imageSource = "res://Default";
+		frameModule.Frame.defaultTransition = { name: "slide" };
 	}
-	else {
-		//image.imageSource = "res://splashscreen.9";
-	}
+	
 	image.animate({
 		opacity: 1,
 		duration: 4000
