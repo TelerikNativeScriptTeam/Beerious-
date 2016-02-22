bed5aa9fe8f957d67afa39c4d6cfdd9e14f226d3
Bianca Hinova <bianca.hinova@gmail.com>
2016-02-22 23:13:50 +0200

    splash screen animation changed

--- app/views/splash/splash.js
+++ app/views/splash/splash.js
@@ -1,28 +1,45 @@
 var frameModule = require("ui/frame");
-var ImageModule = require("ui/image");
+var imageModule = require("ui/image");
 
 exports.loaded = function (argument) {
 	var page = argument.object;
-	var image = page.getViewById("imageSplash");
+	//var item = page.getViewById("logo");
 
 	if(page.ios) {
 		frameModule.Frame.defaultTransition = { name: "slideTop" };
-		image.src = "res://Default.png";
 	}
-	else {
-		image.src = "res://splashscreen.9.png"
-	}
-
-	image.animate({
-		opacity: 1,
-		duration: 3000
-	})
-	.then(function() {
-		var navigationEntry = {
-			moduleName: "views/menu/menu",
-
-		}
-		frameModule.topmost().navigate(navigationEntry);
-		//("views/login/login");
-	});	
-};
+	if (frameModule.topmost().ios) {
+        frameModule.topmost().ios.navBarVisibility = "never";
+    }
+    var item = new imageModule.Image();
+	item.src = "res://splashlogo";
+    item.height = 150;
+    item.on("loaded", function (args) {
+		argument.object
+			.animate({
+				scale: {x: 0.6, y: 0.6},
+				duration: 1500
+			})
+			.then(function (){
+				return argument.object.animate({
+					scale: { x: 7, y: 7 },
+                    duration: 300
+				});
+			})
+			.then(function () {
+				return argument.object.animate({
+                    opacity: 0,
+                    duration: 150
+                });
+			})
+			.then(function () {
+                
+                frameModule.topmost().navigate({
+                    moduleName: "views/main-page/main-page",
+                    animated: false
+                });
+            });
+	});
+	var grid = page.getViewById("grid");
+    grid.addChild(item);
+}
